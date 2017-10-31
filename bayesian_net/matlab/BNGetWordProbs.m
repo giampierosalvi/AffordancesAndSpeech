dc26912b905380a91646a150bc5e432b86a4b7b1
Giampiero Salvi <giampi@kth.se>
2017-10-31 16:42:33 +0100

    added extra argument to choose words from the list

--- bayesian_net/matlab/BNGetWordProbs.m
+++ bayesian_net/matlab/BNGetWordProbs.m
@@ -1,6 +1,12 @@
-function probs = BNGetWordProbs(netobj)
+function probs = BNGetWordProbs(netobj, word_indexes)
+% BNGetWordProbs: returns probabilities of each word in the model
+% 
+% Inputs:
+% netobj: affordance structure created by createBN
+% word_indexes: optional vector of word indexes, default: netobj.WORDNODES
 %
-%
+% Output:
+% probs: vector of probabilities of each word corresponding to word_indexes
 %
 % (C) 2010, Giampiero Salvi, <giampi@kth.se>
 
@@ -9,8 +15,12 @@ if ~isfield(netobj, 'engine') || ...
     error('you have to enter evidence first (see BNEnterEvidence)');
 end
 
-probs = zeros(1,length(netobj.WORDNODES));
-for h=1:length(netobj.WORDNODES)
-    marg = marginal_nodes(netobj.engine, netobj.WORDNODES(h));
+if nargin<2
+    word_indexes = netobj.WORDNODES
+end
+
+probs = zeros(1,length(word_indexes));
+for h=1:length(word_indexes)
+    marg = marginal_nodes(netobj.engine, word_indexes(h));
     probs(h) = marg.T(2); % probability of the word being said
 end
