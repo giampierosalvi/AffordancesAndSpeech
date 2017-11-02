f2381806e86c4d8f12e08887f9f300a28a278d57
Giampiero Salvi <giampi@kth.se>
2017-11-02 16:06:55 +0100

    renamed indexes to indices

--- bayesian_net/matlab/BNGetWordProbs.m
+++ bayesian_net/matlab/BNGetWordProbs.m
@@ -1,12 +1,15 @@
-function probs = BNGetWordProbs(netobj, word_indexes)
+function probs = BNGetWordProbs(netobj, word_indices)
 % BNGetWordProbs: returns probabilities of each word in the model
 % 
 % Inputs:
 % netobj: affordance structure created by createBN
-% word_indexes: optional vector of word indexes, default: netobj.WORDNODES
+% word_indices: optional vector of word indices, default: netobj.WORDNODES
 %
 % Output:
-% probs: vector of probabilities of each word corresponding to word_indexes
+% probs: vector of probabilities of each word corresponding to word_indices
+% 
+% NOTE: if some of the words are given as evidence and are not
+% excluded from word_indices, the returned probability will be 1.0
 %
 % (C) 2010, Giampiero Salvi, <giampi@kth.se>
 
@@ -16,11 +19,11 @@ if ~isfield(netobj, 'engine') || ...
 end
 
 if nargin<2
-    word_indexes = netobj.WORDNODES
+    word_indices = netobj.WORDNODES
 end
 
-probs = zeros(1,length(word_indexes));
-for h=1:length(word_indexes)
-    marg = marginal_nodes(netobj.engine, word_indexes(h));
+probs = zeros(1,length(word_indices));
+for h=1:length(word_indices)
+    marg = marginal_nodes(netobj.engine, word_indices(h));
     probs(h) = marg.T(2); % probability of the word being said
 end
