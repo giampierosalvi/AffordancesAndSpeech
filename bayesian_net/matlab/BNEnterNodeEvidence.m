67fd7d23674dc2e1dfdbd8fcb5509b868039c434
Giampiero Salvi <giampi@kth.se>
2017-11-02 17:23:55 +0100

    added support for soft evidence

--- bayesian_net/matlab/BNEnterNodeEvidence.m
+++ bayesian_net/matlab/BNEnterNodeEvidence.m
@@ -1,39 +1,85 @@
-function [netobj] = BNEnterNodeEvidence(netobj, nodevaluepairs, incremental)
+function [netobj] = BNEnterNodeEvidence(netobj, nodeValuePairs, ...
+                                        incremental, nodeDistPairs)
+% BNEnterNodeEvidence:
+% enter hard or soft evidence by means of node names and value
+% names or probability distributions
 %
+% Inputs:
+% nodeValuePairs: cell array containing pairs of node name and
+%                 value name for the observed nodes (hard evidence).
+% incremental:    retains evidence specified so far (default true)
+% nodeDistPairs:  cell array containing pairs of node name and
+%                 probability distributions for the observed nodes
+%                 (soft evidence)
+% Output:
+% netobj:         updated object
 %
-% nodevaluepairs: cell array pairs of node name and value name for the
-% observed nodes.
+% NOTE: to know the order of values in a node with nodeName, refer
+% to netobj.nodeValueNames{BNWhichNode(nodeName)}
 %
-% incremental: retains evidence specified so far (default true)
+% NOTE: if both nodeValuePairs and nodeDistPairs refer to the same
+% nodes, the second will override the first.
 %
 % Example:
 % netobj = BNEnterNodeEvidence(netobj, {'Color', 'yellow', 'Size', 'big'})
 %
+% netobj = BNEnterNodeEvidence(netobj, {'Color', 'yellow', 'Size',
+% 'big'}, [], {'Action', [0.1 0.3 0.6]})
+%
 % (C) 2010, Giampiero Salvi, <giampi@kth.se>
 
-if nargin < 3
-    incremental = 1;
+if nargin > 2 && incremental == 0
+    netobj = BNResetEvidence(netobj)
 end
 
-if ~incremental || ~isfield(netobj, 'evidence')
-    for h=netobj.ALLNODES
-        netobj.evidence{h}=[];
+% first take care of hard evidence
+for h=1:2:length(nodeValuePairs)
+    nodename = nodeValuePairs(h);
+    nodevalue = nodeValuePairs(h+1);
+    nodeidx = BNWhichNode(netobj, nodename);
+    if isempty(nodeidx)
+        warning('node %s unknown, ignoring\n', nodename{1});
+        continue
+    end
+    valueidx = BNWhichNodeValue(netobj, nodevalue);
+    if isempty(valueidx)
+        warning('value %s not valid for node %s, ignoring\n', ...
+                nodevalue{1}, nodename{1});
+        continue
+    end
+    % everything went fine: update hard evidence
+    netobj.evidence{nodeidx} = valueidx;
+    % ...and, in case remove soft evidence
+    if ~isempty(netobj.soft_evidence{nodeidx})
+        netobj.soft_evidence{nodeidx} = []
     end
 end
 
-for h=1:2:length(nodevaluepairs)
-    nodename = nodevaluepairs(h);
-    nodevalue = nodevaluepairs(h+1);
-    nodeidx = find(strcmpi(nodename, netobj.nodeNames));
-    if isempty(nodeidx)
-        warning('node %s unknown, ignoring\n', nodename{1});
-    else
-        valueidx = find(strcmpi(nodevalue, netobj.nodeValueNames{nodeidx}));
-        if isempty(valueidx)
-            error('value %s not valid for node %s', nodevalue{1}, nodename{1});
-        else
-            netobj.evidence{nodeidx} = valueidx;
+% now take care of soft evidence
+if nargin > 3
+    for h=1:2:length(nodeDistPairs)
+        nodename = nodeDistPairs(h);
+        nodeDist = nodeDistPairs(h+1);
+        nodeidx = BNWhichNode(netobj, nodename);
+        if isempty(nodeidx)
+            warning('node %s unknown, ignoring\n', nodename{1});
+            continue
+        end
+        if length(nodeDist) ~= length(netobj.nodeValueNames{nodeidx})
+            warning('distribution for node %s has wrong length, ignoring\n', nodename{1})
+            continue
+        end
+        if sum(nodeDist)-1 > eps
+            warning('distribution for node %s does not sum to 1, ignoring\n', nodename{1})
+            continue
+        end
+        % everything went fine: update soft evidence
+        netobj.soft_evidence{nodeidx} = nodeDist;
+        % ...and, in case remove hard evidence
+        if ~isempty(netobj.evidence{nodeidx})
+            netobj.evidence{nodeidx} = []
         end
     end
 end
-netobj = BNEnterEvidence(netobj, netobj.evidence, incremental);
+% finally send evidence to engine
+netobj = BNEnterEvidence_private(netobj);
