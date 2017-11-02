87fbdf8b09985379f5a5393cf25eb0d59b300fc5
Giampiero Salvi <giampi@kth.se>
2017-11-02 16:21:13 +0100

    added short description

--- bayesian_net/matlab/BNWhichNode.m
+++ bayesian_net/matlab/BNWhichNode.m
@@ -1,3 +1,6 @@
 function node = BNWhichNode(netobj, nodename)
+% BNWhichNode: returns node index from node name
+%
+% (C) 2010-2017 Giampiero Salvi <giampi@kth.se>
 
 node = find(strcmp(netobj.nodeNames, nodename));
--- bayesian_net/matlab/BNWhichNodeValue.m
+++ bayesian_net/matlab/BNWhichNodeValue.m
@@ -1,3 +1,6 @@
 function idx = BNWhichNodeValue(netobj, node, valuename)
+% BNWhichNodeValue: returns node value index from value name
+%
+% (C) 2010-2017 Giampiero Salvi <giampi@kth.se>
 
 idx = find(strcmp(netobj.nodeValueNames{node}, valuename));
