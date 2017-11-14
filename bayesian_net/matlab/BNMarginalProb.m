3317a3c7720e5676a0ab2c836aa192b3e5dc8c7d
Giampiero Salvi <giampi@kth.se>
2017-11-14 13:59:01 +0100

    correct name for arrayfun

--- bayesian_net/matlab/BNMarginalProb.m
+++ bayesian_net/matlab/BNMarginalProb.m
@@ -1,5 +1,16 @@
 function probdist = BNMarginalProb(netobj, inferenceNodeNames)
+% BNMarginalProb: returns marginal probability distribution
+%   over the nodes in inferenceNodeNames
+% Input:
+% netobj: object as created by createBN
+% inferenceNodeNames: cell array of strings with node names (see netobj.nodeNames)
+% Example:
+% nodeidx = BNWhichNode(netobj, 'Size')
+%
+% See also createBN, BNEnterNodeEvidence
+%
+% (C) 2010-2017 Giampiero Salvi <giampi@kth.se>
 
-predictionNodes = arrfun(@(x) BNWhichNode(netobj,x), inferenceNodeNames);
+predictionNodes = arrayfun(@(x) BNWhichNode(netobj,x), inferenceNodeNames);
 marg = marginal_nodes(netobj.engine, predictionNodes);
-probdist = marg.T
+probdist = marg.T;
